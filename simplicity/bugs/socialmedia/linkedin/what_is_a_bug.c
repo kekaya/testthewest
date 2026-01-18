a5037b6db83a72f34024daa0473c80565e549f3f
Marsi A hosted <gkecht@web.de>
2026-01-18 09:58:01 +0100

    Mal anfangen hier: What is a bug?

---
+++ simplicity/bugs/socialmedia/linkedin/what_is_a_bug.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdint.h>
+
+// Diskussion über bugs auf Linkedin:
+// https://www.linkedin.com/feed/update/urn:li:activity:7418230606125236224?commentUrn=urn%3Ali%3Acomment%3A%28activity%3A7418230606125236224%2C7418565629609361408%29&dashCommentUrn=urn%3Ali%3Afsd_comment%3A%287418565629609361408%2Curn%3Ali%3Aactivity%3A7418230606125236224%29
+// ich mach das hier später weiter, interessante Sache
+//
+
+
+#define DO_NOT_USE_THIS_ILLEGAL_VALUE_IN_THE_DENOMINATOR_OF_A_DIVISON 0
+
+int main(int argc, char **argv){
+  int32_t numerator;
+  int32_t denominator;
+  
+  printf("args :%d\n",argc);
+   
+}
+
